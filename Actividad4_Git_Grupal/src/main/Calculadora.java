f99897d51771ddea0f5d8eb6bb75b8987f5291c7
andres <andreslatasa@gmail.com>
2021-03-08 14:03:56 +0100

    add valor acumulado al metodo suma de la clase main

--- Actividad4_Git_Grupal/src/main/Calculadora.java
+++ Actividad4_Git_Grupal/src/main/Calculadora.java
@@ -8,9 +8,9 @@ public class Calculadora {
 		// suma - andrés
 		Suma suma = new Suma();
 
-		System.out.println("Suma dos números reales: " + suma.SumaDosReales(13.25, 65.50));
-		System.out.println("Suma dos números enteros: " + suma.SumaDosEnteros(50, 60));
-		System.out.println("Suma tres números reales: " + suma.SumaTresReales(100.50, 200.50, 1000.01));
+		System.out.println("Suma dos números reales: " + suma.SumaDosReales(13.25, 65.50) + " Valor acumulado: " + suma.ValorAcumulado());
+		System.out.println("Suma dos números enteros: " + suma.SumaDosEnteros(50, 60)+ " Valor acumulado: " + suma.ValorAcumulado());
+		System.out.println("Suma tres números reales: " + suma.SumaTresReales(100.50, 200.50, 1000.01)+ " Valor acumulado: " + suma.ValorAcumulado());
 
 		// resta - diego
 
