9244f052d2accac867f6a8b411b51e252d81f01e
diego <diegopozo.u@gmail.com>
2021-03-09 09:24:17 +0100

    Resta y main acabdo

--- Actividad4_Git_Grupal/src/main/Calculadora.java
+++ Actividad4_Git_Grupal/src/main/Calculadora.java
@@ -2,6 +2,7 @@ package main;
 
 import pojos.Cociente;
 import pojos.Producto;
+import pojos.Resta;
 import pojos.Suma;
 
 public class Calculadora {
@@ -10,15 +11,23 @@ public class Calculadora {
 		// suma - andrés
 		Suma suma = new Suma();
 
+		System.out.println("**SUMAS**");
 		System.out.println("Suma dos números reales: " + suma.SumaDosReales(13.25, 65.50) + " Valor acumulado: " + suma.ValorAcumulado());
 		System.out.println("Suma dos números enteros: " + suma.SumaDosEnteros(50, 60)+ " Valor acumulado: " + suma.ValorAcumulado());
 		System.out.println("Suma tres números reales: " + suma.SumaTresReales(100.50, 200.50, 1000.01)+ " Valor acumulado: " + suma.ValorAcumulado());
 
 		// resta - diego
+		Resta resta = new Resta();
+
+		System.out.println("\n**RESTAS**");
+		System.out.println("Resta dos números reales: " + resta.RestaDosReales(145.00, 65.50) + " Valor acumulado: " + resta.valorAcumulado());
+		System.out.println("Resta dos números enteros: " + resta.RestaDosEnteros(100, 60)+ " Valor acumulado: " + resta.valorAcumulado());
+		System.out.println("Resta tres números reales: " + resta.RestaTresReales(100.50, 7.50, 10.25)+ " Valor acumulado: " + resta.valorAcumulado());
 
 		// producto - javi
 		Producto producto = new Producto();
 		
+		System.out.println("\n**PRODUCTOS**");
 		System.out.println("Producto dos números reales: " + producto.productoNumerosReales(50.50, 2.50));
 		System.out.println("Producto dos números enteros: " + producto.productoNumerosEnteros(100, 2));
 		System.out.println("Producto tres números reales: " + producto.productoNumerosReales2(50.75, 20.50, 100.01));
@@ -26,6 +35,7 @@ public class Calculadora {
 
 		// división - julia
 		Cociente division = new Cociente();
+		System.out.println("\n**DIVISIONES Y RAIZ CUADRADA**");
 		System.out.println("División dos números reales: " + division.DivisionDosReales(150.50, 5.70));
 		System.out.println("División dos números enteros: " + division.DivisionDosEnteros(70, 2));
 		System.out.println("División inverso real: " + division.InversoReal(150));
--- Actividad4_Git_Grupal/src/pojos/Resta.java
+++ Actividad4_Git_Grupal/src/pojos/Resta.java
@@ -2,46 +2,30 @@ package pojos;
 
 public class Resta {
 
-	private double numRestaR1;
-	private double numRestaR2;
-	private int numRestaE1;
-	private int numRestaE2;
-	private double numRestaR3;
-	private double numRestaR4;
-	private double numRestaR5;
 	private double valorAcumuladoResta;
-	
-	public Resta(double numRestaR1, double numRestaR2, int numRestaE1, int numRestaE2, double numRestaR3,
-			double numRestaR4, double numRestaR5, double valorAcumuladoResta) {
-		this.numRestaR1 = numRestaR1;
-		this.numRestaR2 = numRestaR2;
-		this.numRestaE1 = numRestaE1;
-		this.numRestaE2 = numRestaE2;
-		this.numRestaR3 = numRestaR3;
-		this.numRestaR4 = numRestaR4;
-		this.numRestaR5 = numRestaR5;
-		this.valorAcumuladoResta = valorAcumuladoResta;
+
+	public Resta() {
+
 	}
-	
-	
-	public double RestaDosReales() {
-		double restaDosReales = numRestaR1 - numRestaR2;
+
+	public double RestaDosReales(double a, double b) {
+		double restaDosReales = a - b;
 		valorAcumuladoResta += restaDosReales;
 		return restaDosReales;
 	}
-	
-	public int RestaDosEnteros() {
-		int restaDosEnteros = numRestaE1 - numRestaE2;
+
+	public int RestaDosEnteros(int c, int d) {
+		int restaDosEnteros = c - d;
 		valorAcumuladoResta += restaDosEnteros;
 		return restaDosEnteros;
 	}
-	
-	public double RestaTresEnteros() {
-		double restaTresReales = numRestaR3 - numRestaR4 - numRestaR5;
+
+	public double RestaTresReales(double e, double f, double g) {
+		double restaTresReales = e - f - g;
 		valorAcumuladoResta += restaTresReales;
 		return restaTresReales;
 	}
-	
+
 	public double valorAcumulado() {
 		return valorAcumuladoResta;
 	}
