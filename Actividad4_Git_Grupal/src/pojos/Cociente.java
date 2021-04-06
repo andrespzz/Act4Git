6fac38379953b585fde7d1c77f54fa52fe6072c7
Julia <juliajimenezayuso72@gmail>
2021-04-06 12:31:09 +0200

    AddJavaDocCociente

--- Actividad4_Git_Grupal/src/pojos/Cociente.java
+++ Actividad4_Git_Grupal/src/pojos/Cociente.java
@@ -1,25 +1,54 @@
 package pojos;
 
-public class Cociente {
-	
-	public Cociente() {
-	}
+/**
+ * 
+ * @author Julia
+ * @version 1.0
+ *
+ */
 
+public class Cociente {
+
+	/**
+	 * El metodo <i>DivisionDosReales</i> divide dos numeros reales. </br>
+	 * <b>Ej:</b> 26 <b> :  </b> 2
+	 * @param a Representa el primer numero
+	 * @param b Representa el segundo numero
+	 * @return Devuelve la división de los valores introducidos (a <b>:</b> b).
+	 */
 	public double DivisionDosReales (double a, double b){
 		double DivisionDosReales = a / b;
 		return DivisionDosReales;
 	}
 	
+	/**
+	 * El metodo <i>DivisionDosEnteros</i> divide dos numeros enteros. </br>
+	 * <b>Ej:</b> 10 <b> :  </b> 5
+	 * @param c Representa el primer numero
+	 * @param d Representa el segundo numero
+	 * @return Devuelve la division de los valores introducidos (a <b>:</b> b).
+	 */
 	public double DivisionDosEnteros (double c, double d){
 		double DivisionDosEnteros = c / d;
 		return DivisionDosEnteros;
 	}
 	
+	/**
+	 * El metodo <i>InversoReal</i> calcula el numero inverso. </br>
+	 * @param invR Representa el numero a invertir
+	 * @return Devuelve el numero inverso calculado
+	 */
 	public double InversoReal (double invR) {
-		double DivisionDosEnteros = 1 / invR;
-		return DivisionDosEnteros;
+		double InversoReal = 1 / invR;
+		return InversoReal;
 	}
 	
+	/**
+	 * El metodo <i>RaizNumero</i> calcula la raiz cuadrada de un numero. </br>
+	 * <b>Ej:</b> 2 <b> (2) =  </b> 4
+	 * @param raizn Representa el numero para calcular su raiz
+	 * @return Devuelve la raiz cuadrada calculada
+	 */
 	public double RaizNumero (double raizn) {
 		double RaizNumero = Math.sqrt(raizn);
 		return RaizNumero;
